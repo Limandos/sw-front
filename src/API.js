e7364c23433e300ce357468299d0cb9919e9c39d
DimaS <dmitry_sapr@mail.ru>
2022-07-16 14:35:21 +0400

    Changed fetch result assign.

--- src/API.js
+++ src/API.js
@@ -2,7 +2,7 @@ export async function getData(url) {
     const response = await fetch(url);
     if (response.ok) {
         const json = await response.json();
-        return Object.assign(json, { success: true })
+        return { ...json, succes: true }
     }
     else {
         //console.error(response.statusText)
